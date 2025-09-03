eb7336ec0c45e8cc7989a063f0a7b630ace8d9e4
Marina <marina.paivacm@gmail.com>
2025-09-02 23:16:33 -0300

    Adicionei testes automatizados para a função

---
+++ test/calculadora.tests.js
@@ -0,0 +1,11 @@
+const { somarDoisNumeros } = require('../src/calculadora');
+
+describe('Testes da Função Soma', function () {
+    it('A função deve ser capas de somar dois números positivos', function () {
+
+    });
+
+    it('A função deve ser capaz de somar um número positivo e um negativo', function () {
+        
+    });
+});
